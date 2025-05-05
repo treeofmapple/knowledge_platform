fde7bc73ad68b2e25b354e2037e2524710a9bd07
treeofmapple <samuelsantana778@gmail.com>
2025-05-05 15:01:42 -0300

    first commit

---
+++ src/main/java/com/tom/tool/knowledge/KnowledgePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.tom.tool.knowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowledgePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowledgePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tom/tool/knowledge/KnowledgePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tom.tool.knowledge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowledgePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
